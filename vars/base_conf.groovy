e20f2c5fd80a9661beb1e97d88af4fbd55f18c5b
Rehan Anwar <rehanann@gmail.com>
2020-06-16 14:29:36 +0100

    update code

--- vars/base_conf.groovy
+++ vars/base_conf.groovy
@@ -4,7 +4,7 @@ class Cname{
     @NonCPS
     def conf_tag() {
         def confFile = new groovy.json.JsonSlurperClassic().parse(new File('/var/lib/jenkins/workspace/jenkins-share-demo@libs/jenkins-shared-library/resources/config.json'))
-            conf.branching.each{
+            confFile.branching.each{
             println "${it}"
         }
     }
