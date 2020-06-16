68d09fdcad7b361a40f59ce70970c0e360a9bdd7
Rehan Anwar <rehanann@gmail.com>
2020-06-16 23:00:37 +0100

    update classes

--- src/com/example/Cname.groovy
+++ src/com/example/Cname.groovy
@@ -5,7 +5,7 @@ class Cname{
     @NonCPS
     def confFile() {
         def slurper = new groovy.json.JsonSlurperClassic()
-        def confFile = new groovy.json.JsonSlurperClassic().parse(new File('resources/config.json'))
+        def confFile = new groovy.json.JsonSlurperClassic().parse(new File('/var/lib/jenkins/workspace/jenkins-share-demo@libs/jenkins-shared-library/resources/config.json'))
         return confFile
     }
 }
