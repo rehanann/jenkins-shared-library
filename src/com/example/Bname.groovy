b90acb25479feb8880a4893fdbd0d356ff4d3600
Rehan Anwar <rehanann@gmail.com>
2020-06-16 13:14:31 +0100

    update code

--- src/com/example/Bname.groovy
+++ src/com/example/Bname.groovy
@@ -14,7 +14,7 @@ import groovy.json.JsonSlurperClassic
 class Bname{
     @NonCPS
     def branch_tag() {
-        def slurper = new JsonSlurper()
+        def slurper = new groovy.json.JsonSlurperClassic()
         def branch_name = slurper.parseText('{ "branching": ["cp", "cps", "din", "po", "master", "testsub"] }')
         return branch_name
     }
