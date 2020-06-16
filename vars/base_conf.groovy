d2146ab9a6a68c8cf99cb8b7dea1a8d491ea3c30
Rehan Anwar <rehanann@gmail.com>
2020-06-16 14:19:37 +0100

    update code

--- vars/base_conf.groovy
+++ vars/base_conf.groovy
@@ -3,7 +3,7 @@ import groovy.json.JsonSlurperClassic
 class Cname{
     @NonCPS
     def conf_tag() {
-        def confFile = new groovy.json.JsonSlurperClassic().parse(new File('../resourse/config.json'))
+        def confFile = new groovy.json.JsonSlurperClassic().parse(new File('../resourses/config.json'))
             conf.branching.each{
             println "${it}"
         }
