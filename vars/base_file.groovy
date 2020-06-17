a656df83957cbfe95830ca1f3f0e560e0c9c5646
Rehan Anwar <rehanann@gmail.com>
2020-06-17 09:32:40 +0100

    update classes

--- vars/base_file.groovy
+++ vars/base_file.groovy
@@ -5,9 +5,8 @@ class Dname{
     def myFile() {
         def slurper = new groovy.json.JsonSlurperClassic()
         def confFile = new groovy.json.JsonSlurperClassic().parse(new File('/var/lib/jenkins/workspace/jenkins-share-demo@libs/jenkins-shared-library/resources/config.json'))
-        def D = new Dname()
-            branch = D.myFile()
-            branch.branching.each{
+            // branch = D.myFile()
+            confFile.branching.each{
                 println "${it}"
     }
     }
