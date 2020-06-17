4b6f3d1960aaac7285a24d1ab3cfa285a2fec054
Rehan Anwar <rehanann@gmail.com>
2020-06-17 09:58:28 +0100

    update classes

--- vars/base_file.groovy
+++ vars/base_file.groovy
@@ -1,26 +1,12 @@
 import groovy.json.JsonSlurperClassic
 
-// class Dname{
-//     // @NonCPS
-//     def myFile() {
-//         // def slurper = new groovy.json.JsonSlurperClassic()
-//         // def confFile = new groovy.json.JsonSlurperClassic().parse(new File('/var/lib/jenkins/workspace/jenkins-share-demo@libs/jenkins-shared-library/resources/config.json'))
-//         def confFile = new groovy.json.JsonSlurperClassic().parse(new File('../resources/config.json'))
-//             // branch = D.myFile()
-//             confFile.branching.each{
-//                 println "${it}"
-//         }
-//     }
-// }
 
 class Dname{
     @NonCPS
     def myFile() {
-        // def slurper = new groovy.json.JsonSlurperClassic()
         def confFile = new groovy.json.JsonSlurperClassic().parse(new File('/var/lib/jenkins/workspace/jenkins-share-demo@libs/jenkins-shared-library/resources/config.json'))
-            // branch = D.myFile()
-            confFile.branching.each{
-                println "${it}"
+             confFile.branching.each{
+                return "${it}"
         }
     }
 }
