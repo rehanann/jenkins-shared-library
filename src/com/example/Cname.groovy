70417dbcc258c73555cf1ef6360805913cb49b0c
Rehan Anwar <rehanann@gmail.com>
2020-06-28 16:14:47 +0100

    json update

--- src/com/example/Cname.groovy
+++ src/com/example/Cname.groovy
@@ -5,9 +5,10 @@ class Cname{
     @NonCPS
     def confFile() {
         def slurper = new groovy.json.JsonSlurperClassic()
-        // def DIR = new File('/var/lib/jenkins/workspace/jenkins-share-demo@libs/jenkins-shared-library/resources/')
+        def DIR = new File('/var/lib/jenkins/workspace/jenkins-share-demo@libs/jenkins-shared-library/resources/')
         def dir = 'jenkins-share-demo@libs/resources'
-        def DIR = new File( "${WORKSPACE}" + "${dir}")
+        echo "${workspace}"
+        // def DIR = new File( "${WORKSPACE}" + "${dir}")
         def confFile = new groovy.json.JsonSlurperClassic().parse(new File(DIR, 'config.json'))
         return confFile
     }
